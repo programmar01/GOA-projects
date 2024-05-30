444a0fe21c1a30a693d899cb4941b509c3560416
programmar01 <matedolidze232@gmail.com>
2024-05-30 21:21:46 +0400

    second commit

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log(false && true)
+console.log(false && false)
+console.log(true && true)
+console.log(false && true)
+console.log(false || true)
+console.log(false || false)
+console.log(true || true)
+console.log(false || true)
