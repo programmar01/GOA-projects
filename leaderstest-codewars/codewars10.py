475c0f05647dbc055c3dc7bc46383e7a904ebf54
programmar01 <matedolidze232@gmail.com>
2024-06-02 16:56:30 +0400

    second commit

---
+++ leaderstest-codewars/codewars10.py
@@ -0,0 +1,3 @@
+def variance(numbers): 
+    mean = sum(numbers) / len(numbers)
+    return sum((number - mean) ** 2 for number in numbers) / len(numbers)
---
+++ leaderstest-codewars/codewars9.py
@@ -0,0 +1,7 @@
+def generate_hashtag(s):
+    s = '#' + s.title()
+    s = ''.join(s.split())
+    if len(s) in range(2,141):
+        return s
+    else:
+        return False
